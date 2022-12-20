ffff5215577efbabf6ed5d7719277ecf8f017af6
Sanjeev kumar <sachinsaini19970@gmail.com>
2022-12-20 14:06:05 +0530

    update code

--- src/Components/About.js
+++ src/Components/About.js
@@ -46,7 +46,7 @@ export default function About(props) {
                        </div>
                     </div>
                 </div>
-            </div>
+            </div> 
         </div>
     )
 }
